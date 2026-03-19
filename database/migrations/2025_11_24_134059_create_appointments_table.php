3278c1bb4666b14f0cfb3c4298e953076a805f22
anantapoudyal0477 <anantapoudyal0477@gmail.com>
2026-03-19 19:30:54 +0545

    fix appointment model

--- database/migrations/2025_11_24_134059_create_appointments_table.php
+++ database/migrations/2025_11_24_134059_create_appointments_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
          Schema::create('appointments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade')->nullable();
-            $table->foreignId('doctor_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->nullable();
+            $table->foreignId('doctor_id')->constrained('doctors')->onDelete('cascade');
             $table->date('appointment_date');
             $table->text('message')->nullable(); 
             $table->enum('status', ['Pending', 'Confirmed', 'Completed', 'Cancelled'])->default('Pending');
